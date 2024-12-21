51030cea91b6f699a230fec8e7af2e9284462fdb
Akbar <rizqhaamifta@gmail.com>
2024-12-21 10:39:02 +0700

    mengubah pesan untuk di tampilkan

--- app.test.js
+++ app.test.js
@@ -4,6 +4,6 @@ describe('GET /', () => {
     it('responds with Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello, World!');
+        expect(res.text).toBe('Hello, World! Im back');
     });
 });
